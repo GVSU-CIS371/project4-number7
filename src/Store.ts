70d313495112f8d1504f37ceb8c310406518bbda
xxaleebxx <abertram2021@gmail.com>
2024-03-29 15:24:10 -0400

    Update store to use dictionary

--- src/Store.ts
+++ src/Store.ts
@@ -2,24 +2,18 @@ import { defineStore } from "pinia";
 
 
 interface Recipe {
-    name: string;
     temperature: string;
     creamer: string;
     syrup: string;
     beverage: string;
   }
 
-export const useStore = defineStore("Store", {
-    state: () => ({
-        recipes: [] as Recipe[], // array to hold recipes
-    }),
+interface BeverageState {
+    recipes: Record<string, Recipe>
+}
 
-    actions: {
-        addRecipe(recipe: Recipe) {
-            this.recipes.push(recipe);
-        },
-        findRecipeByName(name: string) {
-            return this.recipes.find((recipe) => recipe.name === name);
-        },
-    },
+export const useStore = defineStore("Store", {
+    state: (): BeverageState => ({
+        recipes: {}, // array to hold recipes
+    }),
 });
